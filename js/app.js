7dd0c378ecd9df1630872b176122dc46d59237c2
DanivonneCaSan <daniela.castillo.sn@gmial.com>
2018-02-14 11:25:13 -0600

    Structure and btn function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-ECHO est  activado.
+var $textArea = document.getElementById('textarea');
+console.log($textArea.value);
+
+$("#send").click(function(){
+  var $content =  textarea.value;
+  console.log($content);
+});
